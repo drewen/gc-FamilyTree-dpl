a066b7c4df6743dfd4b3f11758e845af01efc140
andrew-m-smith <andrew.smith@maxwellhealth.com>
2015-05-14 20:10:22 -0400

    Adds auth, import, and reworks endpoints

--- dpl.py
+++ dpl.py
@@ -1,15 +1,28 @@
-from flask import Flask, jsonify, request, abort
-import sqlite3
 import os
-
+import csv
+import sqlite3
+from functools import wraps
+from flask import Flask, jsonify, request, abort, Response
 
 app = Flask(__name__)
 
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'brothers.db'),
-    DEBUG=False
+    DEBUG=True,
+    USERNAME='admin',
+    PASSWORD='password'
 ))
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 
 def init_db():
@@ -39,7 +52,9 @@ class Brother:
         obj['nickname'] = self.nickname
         obj['big'] = self.big
         obj['year'] = self.year
-        obj['littles'] = self.littles
+        obj['littles'] = []
+        for i in self.littles:
+            obj['littles'].append(i.serialize())
         return obj
 
     def create(self):
@@ -51,6 +66,7 @@ class Brother:
         cur.close()
         conn.commit()
         conn.close()
+        self.littles = self.getLittles()
         return self
 
     def read(self):
@@ -77,6 +93,7 @@ class Brother:
         cur.close()
         conn.commit()
         conn.close()
+        self.littles = self.getLittles()
         return self
 
     def delete(self):
@@ -94,14 +111,14 @@ class Brother:
         conn = _get_conn()
         cur = conn.cursor()
         t = (self.nickname,)
-        cmd = 'SELECT nickname FROM brothers WHERE big = ?'
+        cmd = 'SELECT * FROM brothers WHERE big = ?'
         cur.execute(cmd, t)
         res = cur.fetchall()
         cur.close()
         conn.close()
         littles = []
         for i in res:
-            littles.append(i[0])
+            littles.append(Brother(i[0], i[1], i[2], i[3]))
         return littles
 
 
@@ -115,20 +132,26 @@ def getAllBrothers():
     return brothers
 
 
+@app.route('/dpl/search/<q>', methods=['GET'])
 def search(q):
     conn = _get_conn()
     cur = conn.cursor()
     q = '%' + q + '%'
-    t = (q, q, q)
-    cmd = 'SELECT * FROM brothers WHERE nickname LIKE ? OR name LIKE ? OR year LIKE ?'
+    t = (q, q, q, q)
+    cmd = 'SELECT * FROM brothers WHERE nickname LIKE ? OR name LIKE ? OR big LIKE ? OR year LIKE ?'
     cur.execute(cmd, t)
-    res = cur.fetchall()
+    allBrothers = cur.fetchall()
     cur.close()
     conn.close()
-    return res
+    brothers = []
+    for bro in allBrothers:
+        brothers.append(Brother(bro[0], bro[1], bro[2], bro[3]).serialize())
+    res = {}
+    res['brothers'] = brothers
+    return jsonify(res)
 
 
-@app.route('/dpl/', methods=['GET'])
+@app.route('/dpl/brothers/', methods=['GET'])
 def readAll():
     allBrothers = getAllBrothers()
     brothers = []
@@ -139,14 +162,15 @@ def readAll():
     return jsonify(res)
 
 
-@app.route('/dpl/', methods=['POST'])
+@app.route('/dpl/brothers/', methods=['POST'])
+@requires_auth
 def create():
     req = request.get_json()
     nickname = req.get('nickname', '')
     name = req.get('name', '')
     big = req.get('big', '')
     year = req.get('year', '')
-    if nickname and name and year:
+    if nickname and name:
         try:
             brother = Brother(nickname, name, big, year).create()
             return jsonify(brother.serialize())
@@ -156,7 +180,26 @@ def create():
         abort(400)
 
 
-@app.route('/dpl/<nickname>', methods=['GET'])
+@app.route('/dpl/import/', methods=['POST'])
+@requires_auth
+def uploadCsv():
+    file = request.files['file']
+    ext = os.path.splitext(file.filename)[-1].lower()
+    if file is not None and ext == '.csv':
+        filestream = csv.DictReader(file)
+        brothers = []
+        for row in filestream:
+            bro = Brother(row['Nickname'], row['Name'], row['Big'], row['Year'])
+            if Brother(row['Nickname']).read() is None:
+                bro.create()
+            else:
+                bro.update()
+        return readAll()
+    else:
+        abort(400)
+
+
+@app.route('/dpl/brothers/<nickname>', methods=['GET'])
 def readOne(nickname):
     brother = Brother(nickname).read()
     if brother:
@@ -165,16 +208,16 @@ def readOne(nickname):
         abort(404)
 
 
-@app.route('/dpl/<nickname>', methods=['PUT'])
+@app.route('/dpl/brothers/<nickname>', methods=['PUT'])
+@requires_auth
 def update(nickname):
     brother = Brother(nickname).read()
     if brother:
         req = request.get_json()
-        nickname = req.get('nickname', '')
         name = req.get('name', '')
         big = req.get('big', '')
         year = req.get('year', '')
-        if nickname and name and year:
+        if nickname and name:
             try:
                 brother = Brother(nickname, name, big, year).update()
                 return jsonify(brother.serialize())
@@ -186,7 +229,8 @@ def update(nickname):
         abort(404)
 
 
-@app.route('/dpl/<nickname>', methods=['DELETE'])
+@app.route('/dpl/brothers/<nickname>', methods=['DELETE'])
+@requires_auth
 def delete(nickname):
     brother = Brother(nickname).read()
     if brother:
@@ -202,6 +246,7 @@ def delete(nickname):
 # nickname is the nickname of the big
 # expects little to be the nickname of the little
 @app.route('/dpl/add-little/<nickname>', methods=['PUT'])
+@requires_auth
 def addLittle(nickname):
     req = request.get_json()
     little = req.get('little', None)
@@ -216,6 +261,25 @@ def addLittle(nickname):
             abort(500)
     else:
         abort(404)
+
+
+def check_auth(username, password):
+    return username == app.config['USERNAME'] and password == app.config['PASSWORD']
+
+
+def authenticate():
+    return Response(
+        'Unauthorized', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers['Content-Type'] = 'text/json'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With,Content-Type, Accept, Authorization'
+    return response
 
 
 if __name__ == '__main__':
--- dpl_tests.py
+++ dpl_tests.py
@@ -3,12 +3,33 @@ import dpl
 import unittest
 import tempfile
 import json
+from werkzeug.datastructures import Headers
+from base64 import b64encode
+from StringIO import StringIO
 
-
-Vaporizer = dict(name='Andrew Smith', nickname='Vaporizer', big='McLovin', year=2014)
-Karu = dict(name='Kyle Halstead', nickname='Karu', big='Vaporizer', year=2012)
-Sanctus = dict(name='Michael Higgins', nickname='Sanctus', big='Vaporizer', year=2013)
-Dishficks = dict(name='Sebastian Espinosa', nickname='Dishficks', year=2017)
+Vaporizer = dict(
+    name='Andrew Smith',
+    nickname='Vaporizer',
+    big='McLovin\'',
+    year=2014
+)
+Karu = dict(
+    name='Kyle Halstead',
+    nickname='Karu',
+    big='Vaporizer',
+    year=2012
+)
+Sanctus = dict(
+    name='Michael Higgins',
+    nickname='Sanctus',
+    big='Vaporizer',
+    year=2013
+)
+Dishficks = dict(
+    name='Sebastian Espinosa',
+    nickname='Dishficks',
+    year=2017
+)
 
 
 class DplTestCase(unittest.TestCase):
@@ -17,14 +38,17 @@ class DplTestCase(unittest.TestCase):
         self.db_fd, dpl.app.config['DATABASE'] = tempfile.mkstemp()
         dpl.app.config['DEBUG'] = True
         dpl.app.config['TESTING'] = True
+        dpl.app.config['USERNAME'] = 'admin'
+        dpl.app.config['PASSWORD'] = 'password'
         self.app = dpl.app.test_client()
+        self.auth = {'Authorization': 'Basic ' + b64encode('admin:password')}
         dpl.init_db()
 
     def tearDown(self):
         os.unlink(dpl.app.config['DATABASE'])
 
     def test_empty_db(self):
-        response = self.app.get('/dpl/')
+        response = self.app.get('/dpl/brothers/')
         expected = {}
         expected['brothers'] = []
         assert response.status_code == 200
@@ -32,51 +56,187 @@ class DplTestCase(unittest.TestCase):
         response = self.app.get('/dpl/Vaporizer')
         assert response.status_code == 404
 
+    def test_auth(self):
+        response = self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Vaporizer),
+            content_type='application/json'
+        )
+        expected = 401
+        assert response.status_code == expected
+        response = self.app.put(
+            '/dpl/add-little/Vaporizer',
+            data=json.dumps(dict(little='Dishficks')),
+            content_type='application/json'
+        )
+        assert response.status_code == expected
+
     def test_add_brother(self):
-        response = self.app.post('/dpl/', data=json.dumps(Vaporizer), content_type='application/json')
+        response = self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Vaporizer),
+            content_type='application/json',
+            headers=self.auth
+        )
         expected = Vaporizer
         expected['littles'] = []
         assert json.loads(response.data) == expected
-        response = self.app.get('/dpl/Vaporizer')
+        response = self.app.get('/dpl/brothers/Vaporizer')
         assert response.status_code == 200
         assert json.loads(response.data) == expected
-        response = self.app.get('/dpl/')
+        response = self.app.get('/dpl/brothers/')
         expected = dict(brothers=[expected])
         assert response.status_code == 200
         assert json.loads(response.data) == expected
 
     def test_delete_brother(self):
-        self.app.post('/dpl/', data=json.dumps(Vaporizer), content_type='application/json')
-        self.app.delete('/dpl/Vaporizer')
-        response = self.app.get('/dpl/')
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Vaporizer),
+            content_type='application/json',
+            headers=self.auth
+        )
+        self.app.delete(
+            '/dpl/brothers/Vaporizer',
+            headers=self.auth
+        )
+        response = self.app.get('/dpl/brothers/')
         expected = {}
         expected['brothers'] = []
         assert response.status_code == 200
         assert json.loads(response.data) == expected
 
     def test_littles(self):
-        self.app.post('/dpl/', data=json.dumps(Vaporizer), content_type='application/json')
-        self.app.post('/dpl/', data=json.dumps(Karu), content_type='application/json')
-        self.app.post('/dpl/', data=json.dumps(Sanctus), content_type='application/json')
-        response = self.app.get('/dpl/Vaporizer')
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Vaporizer),
+            content_type='application/json',
+            headers=self.auth
+        )
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Karu),
+            content_type='application/json',
+            headers=self.auth
+        )
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Sanctus),
+            content_type='application/json',
+            headers=self.auth
+        )
+        response = self.app.get('/dpl/brothers/Vaporizer')
         expected = Vaporizer
-        expected['littles'] = ['Karu', 'Sanctus']
+        expected['littles'] = [Karu, Sanctus]
+        expected['littles'][0]['littles'] = []
+        expected['littles'][1]['littles'] = []
         assert response.status_code == 200
         assert json.loads(response.data) == expected
 
     def test_add_little(self):
-        self.app.post('/dpl/', data=json.dumps(Vaporizer), content_type='application/json')
-        response = self.app.post('/dpl/', data=json.dumps(Dishficks), content_type='application/json')
-        response = self.app.get('/dpl/Vaporizer')
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Vaporizer),
+            content_type='application/json',
+            headers=self.auth
+        )
+        response = self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Dishficks),
+            content_type='application/json',
+            headers=self.auth
+        )
+        response = self.app.get('/dpl/brothers/Vaporizer')
         expected = Vaporizer
         expected['littles'] = []
         assert response.status_code == 200
         assert json.loads(response.data) == expected
-        response = self.app.put('/dpl/add-little/Vaporizer', data=json.dumps(dict(little='Dishficks')), content_type='application/json')
-        expected['littles'] = ['Dishficks']
+        response = self.app.put(
+            '/dpl/add-little/Vaporizer',
+            data=json.dumps(dict(little='Dishficks')),
+            content_type='application/json',
+            headers=self.auth
+        )
+        expected['littles'] = [Dishficks]
+        expected['littles'][0]['littles'] = []
+        expected['littles'][0]['big'] = 'Vaporizer'
         assert response.status_code == 200
         assert json.loads(response.data) == expected
 
+    def test_search(self):
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Vaporizer),
+            content_type='application/json',
+            headers=self.auth
+        )
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Karu),
+            content_type='application/json',
+            headers=self.auth
+        )
+        self.app.post(
+            '/dpl/brothers/',
+            data=json.dumps(Sanctus),
+            content_type='application/json',
+            headers=self.auth
+        )
+        response = self.app.get('/dpl/search/Vaporizer')
+        Karu['littles'] = []
+        Sanctus['littles'] = []
+        Vaporizer['littles'] = [Karu, Sanctus]
+        expected = dict(brothers=[Vaporizer, Karu, Sanctus])
+        assert response.status_code == 200
+        assert json.loads(response.data) == expected
+
+    def test_csv_upload(self):
+        csvString = 'Name,Nickname,Big,Year\nAndrew Smith,Vaporizer,McLovin\',2014\nKyle Halstead,Karu,Vaporizer,2012'
+        response = self.app.post('/dpl/import/')
+        assert response.status_code == 401
+        response = self.app.post('/dpl/import/', headers=self.auth)
+        assert response.status_code == 400
+        response = self.app.post(
+            '/dpl/import/',
+            data=dict(file=(StringIO(csvString), 'upload.txt')),
+            content_type='multipart/form-data',
+            headers=self.auth
+        )
+        assert response.status_code == 400
+        response = self.app.post(
+            '/dpl/import/',
+            data=dict(file=(StringIO(csvString), 'upload.csv')),
+            content_type='multipart/form-data',
+            headers=self.auth
+        )
+        Karu['littles'] = []
+        Vaporizer['littles'] = [Karu]
+        expected = dict(brothers=[Vaporizer, Karu])
+        assert response.status_code == 200
+        assert json.loads(response.data) == expected
+        response = self.app.get('/dpl/brothers/')
+        assert json.loads(response.data) == expected
+        csvString = 'Nickname,Name,Year,Big\nVaporizer,Andrew Smoth,2014,McLovin\''
+        response = self.app.post(
+            '/dpl/import/',
+            data=dict(file=(StringIO(csvString), 'upload.csv')),
+            content_type='multipart/form-data',
+            headers=self.auth
+        )
+        Vaporizer['name'] = 'Andrew Smoth'
+        expected = dict(brothers=[Vaporizer, Karu])
+        assert response.status_code == 200
+        assert json.loads(response.data) == expected
+        response = self.app.get('/dpl/brothers/')
+        assert json.loads(response.data) == expected
+
+    def test_bad_endpoints(self):
+        response = self.app.post('/dpl/brothers/spam')
+        assert response.status_code == 405
+        response = self.app.put('/dpl/brothers/')
+        assert response.status_code == 405
+        response = self.app.delete('/dpl/brothers/')
+        assert response.status_code == 405
 
 if __name__ == '__main__':
     unittest.main()
